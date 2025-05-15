ed60cdd77397c7bd99f5b0dbd24afcd1bbac5542
sohaila90 <sohaila.al-khayat@hotmail.com>
2025-05-15 11:08:10 +0200

    Update NumberGuess2.cs

--- GuessTheNumber-CSharp/NumberGuess2.cs
+++ GuessTheNumber-CSharp/NumberGuess2.cs
@@ -6,20 +6,21 @@ public class NumberGuess2
     public void Run()
     {
         Random randomNumber = new Random();
-        int correctNumbers = randomNumber.Next(1, 11);
+        int correctNumbers = randomNumber.Next(1, 1);
         int guess = 0; // for å bruke til å gjette
-        int attempts = 0; // 
+        int attempts = 0; // brukes til hvor mange ganger man har gjettet
         Console.WriteLine("Welcome to: Guess the number");
         Console.WriteLine("Please enter a number between 1 and 10");
         while (true) // denne får det som er under her til å kjøre hele tiden
         {
+            
             string input = Console.ReadLine(); // lagrer det user skriver inn
             // Start(num);
-            guessNumber(input, correctNumbers, attempts); // disse para sender til guessnumber func
+            GuessNumber(input, correctNumbers, attempts, guess); // disse para sender til guessnumber func
         }
     }
 
-    public void guessNumber(string input, int correctNumbers, int attempts) // denne tar imot fra run()
+    private static void GuessNumber(string input, int correctNumbers, int attempts, int guess) // denne tar imot fra run()
     {
 
         int inputAsNum = int.Parse(input); // kommer inn fra string input, her har vi endret fra string til int
